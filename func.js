cc50765e8e27643cd0273ef4cddf76fa9d6670d4
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-18 21:25:03 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -6,6 +6,7 @@ fdk.handle(function(input, ctx){
 	  console.error('error:', error); // Print the error if one occurred
 	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 	  console.log('body:', body);
-	  return {"Auth": body};
+	  return {"body": body,
+			  "error": error};
 });
 })
