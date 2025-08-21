c59dc53f8b4683723caa72a3294d7d81de507e5a
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-21 13:28:01 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,8 +1,16 @@
 const fdk=require('@fnproject/fdk');
 const request = require('request');
 fdk.handle(function(input, ctx){
+	let reqBody;
+	let reqResponse;
+	let reqError;
 	console.log("Call REST");
 	request('https://jsonplaceholder.typicode.com/todos/1', function (error, response, body) {
+		reqBody 	= body;
+		reqResponse	= response;
+		reqError	= error;
 	});
-	return {"body": "hello world"};
+	return {"body": 	reqBody,
+			"response":	reqResponse,
+			"error":	reqError};
 })
